1ff0b9244d2bb5b9f99ac25c429b668abe6bad1d
Roy Jia <roy.jia@outlook.com>
2019-06-07 14:32:43 +0800

    more datetime check case

--- src/main/java/AboutZonedDateTime.java
+++ src/main/java/AboutZonedDateTime.java
@@ -1,13 +1,19 @@
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class AboutZonedDateTime {
 
     static DateTimeZone defaulDateTimeZone = DateTimeZone.getDefault();
+    static final DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyyMMdd");
 
     public static void main(String[] args) {
         System.out.println(defaulDateTimeZone.toString());
@@ -28,5 +34,38 @@ public class AboutZonedDateTime {
         DateTime dateTime = new DateTime(new Date().getTime() + (3600 * 24 * 1000));
 
         System.out.println(dateTime.toString());
+
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, 5);
+        System.out.println(new DateTime(calendar));
+        System.out.println(ZonedDateTime.now());
+
+
+        System.out.println("2019-05-22T19:36:42.368-11:00");
+
+        ZonedDateTime curreZonedDateTime = ZonedDateTime.now();
+        System.out.println(curreZonedDateTime.plusDays(10).toString());
+        System.out.println(curreZonedDateTime.plusDays(13).toString());
+
+        System.out.println(new DateTime().plusMonths(1).toString("yyyyMMdd"));
+
+        System.out.println(new Date().toString());
+
+        System.out.println(ZonedDateTime.of(2020, 7, 12, 3, 0, 0, 0, ZoneId.systemDefault()));
+
+        DateTime startDateTime = DateTime.now();
+        DateTime endDateTime = startDateTime.plusMinutes(16);
+
+        long mills = endDateTime.getMillis() - startDateTime.getMillis();
+        System.out.println(mills % 900000);
+
+        System.out.println("--------------------------------");
+
+        Integer datestr = Integer.valueOf("20190605");
+
+        DateTime dateTime2 = DateTime.parse(datestr.toString(), dateTimeFormatter);
+        System.out.println(dateTime2.toString());
+
     }
 }
