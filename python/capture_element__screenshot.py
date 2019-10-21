0d75b8c288ddc79fc79c983d17f8faa1a2dbf1fb
Roy Jia <roy.jia@outlook.com>
2019-10-21 17:05:46 +0800

    update

--- python/capture_element__screenshot.py
+++ python/capture_element__screenshot.py
@@ -9,14 +9,16 @@ from selenium.webdriver.chrome.options import Options
 #########################################################################################################
 # scraper page and capture screenshot
 opts = Options()
+chromedriver = os.path.abspath(os.path.join(os.path.dirname(__file__), "resources/chromedriver/mac/chromedriver"))
 # if we want to capture full page as screenshot, selenium is only working on headless mode
 opts.add_argument('--headless')
 opts.add_argument('--window-size=1200,800')
-driver = webdriver.Chrome(executable_path='resources/chromedriver/mac/chromedriver', options=opts)
+driver = webdriver.Chrome(executable_path=chromedriver, options=opts)
 
 driver.get("http://www.expedia.com")
 
-driver.save_screenshot("aaaaa.png")
+aaaaapng = os.path.abspath(os.path.join(os.path.dirname(__file__), "temp_imgs/aaaaa.png"))
+driver.save_screenshot(aaaaapng)
 
 
 # get snapshot for wizard
@@ -30,14 +32,16 @@ width = location['x']+size['width'];
 height = location['y']+size['height'];
 
 print(f"x: {x}, y: {y}, width: {width}, height: {height}")
+print(element.rect)
 
 size = 1200, 800
 
-im = Image.open('aaaaa.png')
+im = Image.open(aaaaapng)
 im.thumbnail(size)
 print(f"im size: {im.size}")
 im = im.crop((int(x), int(y), int(width), int(height)))
-im.save('wizard.png')
+wizardpng = os.path.abspath(os.path.join(os.path.dirname(__file__), "temp_imgs/wizard.png"))
+im.save(wizardpng)
 
 
 driver.quit()
--- python/capture_full_page_screenshot.py
+++ python/capture_full_page_screenshot.py
@@ -23,13 +23,13 @@ current_width = driver.execute_script("return document.body.clientWidth")
 print("this is current_width: {current_width}".format(current_width=current_width))
 
 print(driver.get_window_size())
-driver.save_screenshot("homepage_before_height.png")
+driver.save_screenshot("temp_imgs/homepage_before_height.png")
 
 driver.set_window_size(1200, current_height)
 print(driver.get_window_size())
 # it would be better that time.sleep(2) here to ensure the page full loaded
 time.sleep(2)
-driver.save_screenshot("homepage_after_height.png")
+driver.save_screenshot("temp_imgs/homepage_after_height.png")
 
 driver.close()
 
@@ -40,5 +40,5 @@ try:
 except FileNotFoundError:
     print(f"file homepage_after_heightssss.png is not exist")
 else:
-    image.convert('RGB').save('homepage_after_height.jpg')
+    image.convert('RGB').save('temp_imgs/homepage_after_height.jpg')
     os.remove("homepage_after_height.png")
