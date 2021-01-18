8df15966f98a3942004e8f55c1886995d6f86504
Roy Jia <roy.jia@outlook.com>
2021-01-18 23:39:50 +0800

    Update RedisCheck.java

--- Java/src/main/java/RedisCheck.java
+++ Java/src/main/java/RedisCheck.java
@@ -7,19 +7,19 @@ public class RedisCheck {
     private static final String REDIS_SCORE_CHECK_KEY = "rjia:test:";
 
     public static void main(String[] args) {
-//        addItem(10, "Alibaba");
-//        addItem(9, "Tencent");
-//        addItem(8, "Baidu");
-//        addItem(7, "HuaWei");
-//        addItem(6, "Netease");
+        addItem(10, "Alibaba");
+        addItem(9, "Tencent");
+        addItem(8, "Baidu");
+        addItem(7, "HuaWei");
+        addItem(6, "Netease");
 
         Set<String> set = getAllItems();
         System.out.println(set);
 
         try (Jedis jedis = new Jedis("localhost")) {
             System.out.println("------" + jedis.zscore(REDIS_SCORE_CHECK_KEY, "Alibaba"));
-            jedis.zadd(REDIS_SCORE_CHECK_KEY, 99, "Alibaba");
-            System.out.println("------" + jedis.zscore(REDIS_SCORE_CHECK_KEY, "Alibaba"));
+//            jedis.zadd(REDIS_SCORE_CHECK_KEY, 99, "Alibaba");
+//            System.out.println("------" + jedis.zscore(REDIS_SCORE_CHECK_KEY, "Alibaba"));
         } catch (Exception e) {
             e.printStackTrace();
         }
